b2a3c811ce996f13d08a6afa9ce25261ce520b4a
Gergely Topolyai <gtopolyai@hortonworks.com>
2020-08-03 16:04:00 +0200

    CB-8100 Missing correct log messages when json deserialization error occurred

--- common/src/main/java/com/sequenceiq/cloudbreak/common/anonymizer/AnonymizerUtil.java
+++ common/src/main/java/com/sequenceiq/cloudbreak/common/anonymizer/AnonymizerUtil.java
@@ -2,7 +2,12 @@ package com.sequenceiq.cloudbreak.common.anonymizer;
 
 import java.util.regex.Pattern;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class AnonymizerUtil {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(AnonymizerUtil.class);
 
     public static final String REPLACEMENT = "$1****";
 
@@ -26,6 +31,7 @@ public class AnonymizerUtil {
     public static String anonymize(String content) {
         String ret = content;
         if (ret != null) {
+            LOGGER.info("Anonymize the content with length: {}", content.length());
             for (ReplacePattern pattern : PATTERNS) {
                 ret = pattern.replaceAll(ret);
             }
